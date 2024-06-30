a91600a7d8e0364aa7145ff8b251247da3acc1b6
Gab0CS <gabocanseco@gmail.com>
2024-06-29 23:46:26 -0600

    Front-end started, navbar finished

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import Image from 'next/image';
+import logo from '../../public/FFicon.png'
+import support from '../../public/Vector.png'
+import { IoIosArrowDown } from "react-icons/io";
+import { FaCircle } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <nav className='flex justify-between h-16 items-center' style={{ background: '#404041', color: '#fff'}}>
+      <div className='flex-start px-[30px] flex items-center'>
+        <Image 
+            src={logo}
+            width={40}
+            height={40}
+            className='mx-2'
+            alt='Fountain forward logo'
+        />
+        <h1 className='text-lg'>
+          Admin Console
+        </h1>
+        <p className='mx-2.5 text-xs px-4 py-1 rounded-3xl'
+        style={{background: '#ffffff', color: '#000000'}}>
+          ADMIN VIEW
+        </p>
+      </div>
+      <div 
+      className='flex px-[30px] items-center'>
+        <div className=''>
+
+        </div>
+        <Image 
+          src={support}
+          width={20}
+          height={20}
+          alt='support vector icon'
+          className='mx-2'
+        />
+
+        <p>Support</p>
+
+        <div style={{ background: '#242424'}}
+          className='mx-4 py-3 rounded-md flex items-center justify-items-end'>
+          <FaCircle 
+          className='mx-3'
+          size={30}
+          style={{ color: '#D9D9D9' }}
+          />
+          Jane
+          <IoIosArrowDown 
+          className='mt-1 mx-3' 
+          style={{ color: 'orange' }}/>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,20 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        'bg-navbar': '#404041',
+      },
+      height: {
+        '10p': '10%',
+        '20p': '20%',
+        '30p': '30%',
+        '40p': '40%',
+        '50p': '50%',
+        '60p': '60%',
+        '70p': '70%',
+        '80p': '80%',
+        '90p': '90%',
+        '100p': '100%',
       },
     },
   },
